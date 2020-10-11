0b41c068f14b79a49ea0fb97f93b7330882ed9a5
Oleg Lavrovsky <oleg@datalets.ch>
2020-10-11 18:15:19 +0200

    Indicator sync

--- dribdat/static/js/script.js
+++ dribdat/static/js/script.js
@@ -42,7 +42,7 @@
         .removeClass('fa-circle-o fa-check-circle-o')
         .addClass(!supported ? 'fa-circle-o' : 'fa-check-circle-o')
         .css('color', (supported ? 'green' : 'red'));
-      $ind.find('.autotext-indicator')
+      $ind
         .css('visibility', (supported ? '' : 'hidden'));
       $('#is_autoupdate').click(function() {
         if ($(this).is(':checked'))
