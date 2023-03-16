4e10ce85f5a1817ee36ca3c01679ec215dfa1ab4
Oleg Lavrovsky <oleg@datalets.ch>
2023-03-16 12:34:12 +0100

    Alert close

--- dribdat/static/js/notify.js
+++ dribdat/static/js/notify.js
@@ -3,19 +3,22 @@
   // Create a notification with the given title
   function createNotification() {
     $.get('/api/event/current/get/status', function(result) {
+      //console.log(result);
       if (result.status) {
         let eventStatus = localStorage.getItem('eventstatus');
         if (eventStatus == result.status) return;
+
         eventStatus = result.status;
         localStorage.setItem('eventstatus', eventStatus);
 
         // Create and show the notification
         window.alert(eventStatus);
+        $('#notifications-status-text').html(eventStatus);
       }
     });
   };
 
-  if (!location.href.indexOf('/dashboard')) {
+  if (location.href.indexOf('/dashboard')<0) {
     createNotification();
     setInterval(createNotification, 30 * 1000); // check once a minute
   }
