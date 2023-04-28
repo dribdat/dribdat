c30be4a742ebda26d72b78ca2f503148f4e32222
Oleg Lavrovsky <loleg@users.noreply.github.com>
2023-04-28 13:26:46 +0200

    Numerise order

--- cli.py
+++ cli.py
@@ -40,16 +40,25 @@ def socialize(kind):
 @click.argument('challenges', required=False, default=True)
 def numerise(event: int, clear: bool, primes: bool, challenges: bool):
     """Assign numbers to challenge hashtags for an EVENT ID."""
+    print("Applying numbers to event #%d")
+    if clear:
+        print("- Clearing hashtags")
+    if primes:
+        print("- Using prime numbers")
+    if challenges:
+        print("- Only challenges")
+    else:
+        print("- Only projects")
     # TODO: use a parameter for sort order alphabetic, id-based, etc.
     if primes:
         # Generate some primes, thanks @DhanushNayak
-        nq = list(filter(lambda x: not list(filter(lambda y : x%y==0, range(2,x))),range(2,200)))
+        nq = list(filter(lambda x: not list(filter(lambda y : x%y==0, range(2,x))), range(2,200)))
     else:
         nq = list(range(1,200))
     with create_app().app_context():
         from dribdat.user.models import Event
-        projects = Event.query.filter_by(id=event) \
-                   .first_or_404().projects
+        projects = Event.query.filter_by(id=event).first_or_404().projects
+        projects = projects.order_by(Project.progress.desc()).order_by(Project.name)
         ix = 0
         for c in projects:
             if c.is_hidden: continue
