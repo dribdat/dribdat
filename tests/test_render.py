3e0f4280975da7bcb6f42adb52c32f3964ec758b
Oleg Lavrovsky <oleg@datalets.ch>
2023-05-10 23:05:52 +0200

    Render test

--- tests/test_render.py
+++ tests/test_render.py
@@ -10,10 +10,10 @@ class TestRender:
 
     def test_datapackage(self, testapp):
         """Render a data package."""
-        test_url = '📦 Airport Codes: [datapackage.json](https://bucketeer-e34189e0-4cb3-496d-a72e-2dcf17cab858.s3.amazonaws.com/cividi/1/KUDZR3KIL28EPKUXI77VVJ45/datapackage.json)'  # noqa: E501
+        test_url = '📦 Data: [datapackage.json](https://meta.dribdat.cc/api/event/2/datapackage.json)'  # noqa: E501
         dpkg_html = box_datapackage(test_url)
         assert "boxout" in dpkg_html
-        assert "Airport Codes" in dpkg_html
+        assert "Awesome hackathon" in dpkg_html
 
     def test_ckan(self, testapp):
         """Render a data package."""
