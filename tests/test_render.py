0627316302e9cc74a6bb97ec75abc60dadfe8b03
Oleg Lavrovsky <loleg@users.noreply.github.com>
2023-06-05 12:26:37 +0200

    Poetry update

--- tests/test_render.py
+++ tests/test_render.py
@@ -4,14 +4,14 @@
 from dribdat.boxout.datapackage import box_datapackage
 from dribdat.boxout.ckan import box_dataset
 
-
 class TestRender:
     """Here be render tests."""
 
     def test_datapackage(self, testapp):
-        """Render a data package."""
+        """Render a remote data package."""
         test_url = '📦 Data: [datapackage.json](https://meta.dribdat.cc/api/event/2/datapackage.json)'  # noqa: E501
         dpkg_html = box_datapackage(test_url)
+        assert dpkg_html is not None
         assert "boxout" in dpkg_html
         assert "Awesome hackathon" in dpkg_html
 
@@ -19,4 +19,5 @@ class TestRender:
         """Render a dataset from CKAN."""
         test_url = 'https://opendata.swiss/de/dataset/21st-century-swiss-video-games'  # noqa: E501
         dpkg_html = box_dataset(test_url)
+        assert dpkg_html is not None
         assert "boxout" in dpkg_html
