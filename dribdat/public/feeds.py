9a95dc4d1589e421c330cdcd3fb489e671513df9
Oleg Lavrovsky <loleg@users.noreply.github.com>
2023-11-28 21:36:40 +0100

    Pub feed format

--- dribdat/public/feeds.py
+++ dribdat/public/feeds.py
@@ -27,18 +27,23 @@ def pubonify(apub):
     return resp
 
 
-@blueprint.route('/u/<username>', methods=['GET'])
+@blueprint.route('/pub/<username>', methods=['GET'])
 def get_user(username):
     """Output ActivityPub with public data by username."""
     a_user = User.query.filter_by(username=username).first_or_404()
     private_key, public_key = a_user.get_keys()
     un = a_user.username
-    url_user = url_for("public.user", username=un, _external=True)
+    url_user = url_for("feed.get_user", username=un, _external=True)
     url_inbox = url_for("feed.post_user_inbox", username=un, _external=True)
     url_outbox = url_for("feed.get_user_activities", username=un, _external=True)
+    url_profile = url_for("public.user", username=un, _external=True)
     apub = {
-        "@context": "https://www.w3.org/ns/activitystreams",
+        "@context": [
+            "https://www.w3.org/ns/activitystreams",
+            "https://w3id.org/security/v1",
+        ],
         "id": url_user,
+        "url": url_profile,
         "inbox": url_inbox,
         "outbox": url_outbox,
         "type": "Person",
@@ -46,47 +51,64 @@ def get_user(username):
         "preferredUsername": un,
         "publicKey": {
             "id": url_user + "#main-key",
+            "owner": url_user,
             "publicKeyPem": public_key
         }
     }
+    if a_user.my_story or a_user.my_goals:
+        apub["summary"] = a_user.my_story or a_user.my_goals
+    if a_user.carddata:
+        if a_user.carddata.endswith('.png'):
+            apub["mediaType"] = "image/png"
+        elif a_user.carddata.endswith('.jpeg') \
+            or a_user.carddata.endswith('.jpg'):
+            apub["mediaType"] = "image/jpeg"
+        apub["icon"] = {
+            "type": "Image",
+            "url": a_user.carddata
+        }
     return pubonify(apub)
 
 
-@blueprint.route('/u/<username>/inbox', methods=['POST'])
+@blueprint.route('/pub/<username>/inbox', methods=['POST'])
 def post_user_inbox(username):
     current_app.logger.info(request.headers)
     current_app.logger.info(request.data)
     return Response("", status=202)
 
 
-@blueprint.route('/u/<username>/activities', methods=['GET'])
+@blueprint.route('/pub/<username>/activities', methods=['GET'])
 def get_user_activities(username, limit=10):
     a_user = User.query.filter_by(username=username).first_or_404()
-    url_user = url_for("public.user", username=a_user.username, _external=True)
+    url_user = url_for("feed.get_user", username=username, _external=True)
+    url_activity = url_for("feed.get_user_activities", username=username, _external=True)
     # Fetch the latest activities
     act_data = []
     for a in a_user.latest_posts(10):
-        url_project = url_for("project.project_view", project_id=a['project_id'])
+        url_project = url_for("project.project_view", project_id=a['project_id'], _external=True)
         url_project = url_project + '?activity=' + str(a['id'])
         act_data.append(
             {
               "@context": "https://www.w3.org/ns/activitystreams",
+              
+              "id": url_activity + '#create-' + str(a['id']),
               "type": "Create",
-              "id": url_project + '#log',
               "actor": url_user,
+
               "object": {
                 "to": "https://www.w3.org/ns/activitystreams#Public",
-                "id": url_project,
+
+                "id": url_activity + '#' + str(a['id']),
                 "type": "Note",
                 "published": a['date'],
                 "attributedTo": url_user,
-                "content": a['content'],
-              },
+                "content": a['content'] + '\n' + url_project,
+              }
             }
         )
     apub = {
       "@context": "https://www.w3.org/ns/activitystreams",
-      "summary": username + ' - dribs',
+      "summary": username + ' - Latest Dribs',
       "type": "OrderedCollection",
       "orderedItems": act_data,
       "totalItems": len(act_data),
