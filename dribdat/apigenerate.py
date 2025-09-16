e258b6e147af0eb83b9c3ca8d196989677e97ad1
Oleg Lavrovsky <loleg@users.noreply.github.com>
2025-09-16 11:58:32 +0200

    Improved prompts and evaluations

--- dribdat/apigenerate.py
+++ dribdat/apigenerate.py
@@ -11,39 +11,48 @@ from .user.models import Project
 REQUEST_TIMEOUT = 30
 
 # Default system prompt for our requests
-SYSTEM_PROMPT = (
-    "A hackathon project involves: "
-    + "collecting data, designing a solution, and making a prototype. "
-    + "Typical stages include team formation, role play, brainstorming, "
-    + "design thinking, and prototyping with digital and physical tools."
-    + "Be brief, wise and positive in your statements. Encourage fairness."
-)
+SYSTEM_PROMPT = """
+A hackathon project involves:
+- collecting data, designing a solution, and making a prototype
+- working through stages of team formation, role play, brainstorming
+- applying design thinking, prototyping with paper and digital tools
+"""
 
 # Default challenge prompts
-INITIAL_CHALLENGE_PROMPT = (
-    "Write a challenge statement for a collaborative hackathon project. "
-    + "Include a section with a basic introduction and first steps. "
-    + "Describe in a second section some example datasets and resources. "
-    + "The last section should explain what kind of skills are involved. "
-    + "Use dashes (---) to separate the sections."
-)
-INITIAL_PROJECT_PROMPT = (
-    "Suggest one clear and concise next step for a hackathon project."
-)
+INITIAL_CHALLENGE_PROMPT = """
+Write a challenge statement for a collaborative hackathon project.
+Include a section with a basic introduction and first steps.
+Describe in a second section some example datasets and resources.
+The last section should explain what kind of impact is expected.
+Use dashes (---) to separate the sections. Each section is one paragraph.
+"""
+
+# Default next project step prompt
+INITIAL_PROJECT_PROMPT = """
+Suggest one clear and concise (100 words or less) next step for a hackathon project.
+"""
 
 # If no other stage advice is found
 DEFAULT_STAGE_ADVICE = "Be excellent to each other"
 
-DEFAULT_EVALUATION_PROMPT = (
-    "You are a judge in a hackathon."
-    + "Generate a short (100 words or less) evaluation of a project,"
-    + "focusing on clarity and sustainability."
-)
+DEFAULT_EVALUATION_PROMPT = """
+You are an experienced judge in a collaborative hackathon.
+Write a short (200 words or less) evaluation of a project.
+Be wise and constructively critical in your statements.
+--- Evaluation criteria ---
+Review the clarity of the documentation.
+%s
+--- Project documentation ---
+%s
+"""
 
-DEFAULT_RECOMMENDATION_PROMPT = (
-    "Generate a short (100 words or less) "
-    + "suggestion as a next step in a hackathon project."
-)
+DEFAULT_RECOMMENDATION_PROMPT = """
+Generate a short (100 words or less) suggestion as a next step, for the
+hackathon project outlined in the section that follows.
+Be wise and constructively critical in your statements.
+--- Project documentation ---
+%s
+"""
 
 # CHAT_TEMPLATE_1 = "{% if messages[0]['role'] == 'system' %}{% set loop_messages = messages[1:] %}{% set system_message = messages[0]['content'] %}{% else %}{% set loop_messages = messages %}{% set system_message = false %}{% endif %}{% for message in loop_messages %}{% if (message['role'] == 'user') != (loop.index0 % 2 == 0) %}{{ raise_exception('Conversation roles must alternate user/assistant/user/assistant/...') }}{% endif %}{% if loop.index0 == 0 and system_message != false %}{% set content = '<<SYS>>\n' + system_message + '\n<</SYS>>\n\n' + message['content'] %}{% else %}{% set content = message['content'] %}{% endif %}{% if message['role'] == 'user' %}{{ bos_token + '[INST] ' + content + ' [/INST]' }}{% elif message['role'] == 'assistant' %}{{ ' '  + content + ' ' + eos_token }}{% endif %}{% endfor %}"
 
@@ -103,6 +112,25 @@ def prompt_ideas(project: Project):
     return basep + "\n\n%s\n\n%s" % (stage_advice, summary)
 
 
+def prompt_summary(project: Project):
+    """Form a prompt that is used to evaluate a project."""
+    summary = "\n\n# README\n\n".join((project.longtext, project.autotext))
+    # Collect project contents
+    latest = []
+    for d in project.latest_dribs(10):
+        dc = d.content or ''
+        if "🅰️ℹ️" in dc:
+            continue
+        latest.append(dc)    
+    dribs = "\n\n".join(latest)
+    # Truncate to a reasonable length
+    trunc = "\n\n#LOG\n\n".join((summary[:1024], dribs[:1024]))
+    return (
+        project.event.aftersubmit or "",
+        trunc,
+    )
+
+
 def gen_project_pitch(project: Project):
     """Returns results from a prompt used for a seed pitch in a project."""
     return gen_project_post(project, False)
@@ -113,14 +141,15 @@ def gen_project_post(project: Project, as_boost: bool = False):
     prompt = " " + prompt_ideas(project)
     if as_boost:
         # Use an evaluation type prompt
-        prompt = DEFAULT_EVALUATION_PROMPT + prompt
+        prompt = DEFAULT_EVALUATION_PROMPT % prompt_summary(project)
     elif project.is_challenge:
         # The challenge prompt will be applied later
         pass
     else:
         # Use the standard recommendation prompt
-        prompt = DEFAULT_RECOMMENDATION_PROMPT + prompt
-    # print(prompt)
+        prompt = DEFAULT_RECOMMENDATION_PROMPT % prompt_ideas(project)
+    # Debugging:
+    ### print(prompt)
     return gen_openai(prompt)
 
 
@@ -158,7 +187,7 @@ def gen_openai(prompt: str):
         j = r.json()
         if "choices" in j:
             content = j["choices"][0]["message"]["content"]
-            return "%s\n\n🅰️ℹ️ Written with help of `%s`" % (content, llm_title)
+            return "%s\n\n🅰️ℹ️ `generated with %s`" % (content, llm_title)
         else:
             logging.error(r.text)
             return None
@@ -216,7 +245,7 @@ def gen_openai(prompt: str):
     # Return the obtained result
     if completion is not None and len(completion.choices) > 0:
         content = completion.choices[0].message.content or ""
-        return "%s\n\n🅰️ℹ️ Written with help of `%s`" % (content, llm_title)
+        return "%s\n\n🅰️ℹ️ `generated with %s`" % (content, llm_title)
     else:
         logging.error("No LLM data in response")
         return None
