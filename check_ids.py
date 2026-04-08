ef526c7e842447eb619a4d50d319ec88e5f3942a
Oleg Lavrovsky <loleg@users.noreply.github.com>
2026-04-08 15:18:19 +0200

    Cleaned up test files

--- check_ids.py
+++
@@ -1,9 +0,0 @@
-from dribdat.app import init_app
-from dribdat.settings import DevConfig
-from dribdat.user.models import User, Event, Project, Resource
-
-app = init_app(DevConfig)
-with app.app_context():
-    for u in User.query.all(): print(f"User: {u.id} {u.username}")
-    for e in Event.query.all(): print(f"Event: {e.id} {e.name}")
-    for p in Project.query.all(): print(f"Project: {p.id} {p.name}")
